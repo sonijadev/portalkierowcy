bcce19deec6adffc3cd171b28d0b7deb1efeeea6
radoradeon <radoslawhirek@gmail.com>
2023-12-01 19:55:54 +0100

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Portal Kierowcy') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <style>
+        .footerlink {text-decoration: none;} footer{position: absolute; bottom: 0; left: 50%; transform: translate(-50%, 0);}
+        </style>
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="https://workflow.sonija.pl/BIURO/Grafika/portalKierowcyLogo.png" alt="CITY PARKING GROUP S.A. LOGO" width="180" height="60">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Logowanie') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Rejestracja') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+    <div class="b-example-divider"></div>
+
+    <footer class="my-5 pt-5 text-muted text-center text-small">
+      <p class="mb-1">© @if (date('Y') != '2023') 2023 - @endif {{ date('Y') }} City Parking Group S.A.</p>
+      @php
+        $example = [
+          [
+            'text' => 'Regulamin',
+            'href' => '#',
+          ],
+          [
+            'text' => 'Polityka prywatności',
+            'href' => '#',
+          ],
+          [
+            'text' => 'Polityka cookies',
+            'href' => '#',
+          ],
+          [
+            'text' => 'O autorze',
+            'href' => '#',
+          ],
+        ]
+      @endphp
+
+      <ul class="list-inline">
+        @foreach($example as $item)
+          <li class="list-inline-item">
+            <a href="{{ $item['href'] }}" target="_blank" class="footerlink">{{ $item['text'] }}</a>
+          </li>
+        @endforeach
+      </ul>
+      
+    </footer>
+</body>
+</html>
---
+++ resources/views/portal.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Portal Kierowcy') }}</div>
+
+                <div class="card-body">
+
+                    {{ __('Zaloguj się aby móc przeglądać treść') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
